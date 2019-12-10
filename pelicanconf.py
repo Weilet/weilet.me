160a7968c872ff97ed6a09192920cfeb02423568
Weilet <453546129@qq.com>
2019-12-10 09:55:40 +0800

    first commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,21 +4,26 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Weilet'
 SITENAME = "Weilet's Workshop"
-SITEURL = ''
+STATIC_PATHS = ['images', 'extra']
+SITELOGO = '/images/rick.png'
+SITESUBTITLE = 'Don\'t be evil'
+FAVICON = '/images/favicon.ico'
+SITEURL = 'https://weilet.github.io'
+
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = u'zh_CN'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-AUTHOR_FEED_RSS = 'feeds/{slug}.rss.xml'
+FEED_ALL_RSS = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),)
@@ -32,4 +37,17 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'elegant-5.1.0'
+THEME = 'Flex'
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
+SOCIAL = (
+    ('github', 'https://weilet.github.io'),
+    ('rss', 'https://weilet.github.io/feeds/all.rss.xml'),
+)
+
+SUMMARY_MAX_LENGTH = 10
