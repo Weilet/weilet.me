6e17bb191fcd155c2da3164174d8ade676cac2eb
Weilet <453546129@qq.com>
2019-12-10 21:17:20 +0800

    first commit

---
+++ content/pages/landing-page-about-me.md
@@ -0,0 +1,37 @@
+---
+author: Admin
+title: 
+layout: page
+date: 2019-01-14 7:30:47 +0100
+status: hidden
+slug: landing-page-about-hidden
+---
+
+
+>若批判无自由，则赞美无意义
+
+### Recently Watching
+
+- Rick and Morty Season 4
+- The Mandalorian Season 1
+- 僕のヒーローアカデミア 第4期
+
+### Recently Reading
+
+- 《动物庄园》
+- 《少数派报告》
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,53 +1,157 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
-AUTHOR = 'Weilet'
+AUTHOR = "Weilet"
 SITENAME = "Weilet's Workshop"
-STATIC_PATHS = ['images', 'extra']
-SITELOGO = '/images/rick.png'
-SITESUBTITLE = 'Don\'t be evil'
-FAVICON = '/images/favicon.ico'
-SITEURL = 'https://weilet.github.io'
+SITESUBTITLE = "Don't be evil"
+SITEURL = ""
 
+PATH = "content"
 
-PATH = 'content'
+# Regional Settings
+TIMEZONE = "Asia/Shanghai"
+DATE_FORMATS = {"en": "%b %d, %Y"}
 
-TIMEZONE = 'Asia/Shanghai'
-
-DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-FEED_ALL_RSS = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-
-THEME = 'Flex'
-
-CC_LICENSE = {
-    'name': 'Creative Commons Attribution-ShareAlike',
-    'version': '4.0',
-    'slug': 'by-sa'
+# Plugins and extensions
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.admonition": {},
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.toc": {"permalink": " "},
+    }
 }
 
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = [
+    "extract_toc",
+    "liquid_tags.img",
+    "neighbors",
+    "related_posts",
+    "render_math",
+    "series",
+    "share_post",
+    "tipue_search",
+]
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
+}
+
+# Appearance
+THEME = "elegant"
+TYPOGRIFY = True
+DEFAULT_PAGINATION = False
+
+# Defaults
+DEFAULT_CATEGORY = "Miscellaneous"
+USE_FOLDER_AS_CATEGORY = False
+ARTICLE_URL = "{slug}"
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}.html"
+TAGS_URL = "tags"
+CATEGORIES_URL = "categories"
+ARCHIVES_URL = "archives"
+SEARCH_URL = "search"
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
+
+# Feeds
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+
+# Social
 SOCIAL = (
-    ('github', 'https://weilet.github.io'),
-    ('rss', 'https://weilet.github.io/feeds/all.rss.xml'),
+    ("Github", "https://github.com/Pelican-Elegant/", "Elegant Github Repository"),
+    ("RSS", SITEURL + "/feeds/all.atom.xml"),
+    (
+        "Calendar",
+        "https://github.com/Pelican-Elegant/elegant/milestones",
+        "Elegant Project Roadmap",
+    ),
 )
 
-SUMMARY_MAX_LENGTH = 10
+# Elegant theme
+STATIC_PATHS = ["theme/images", "images", "extra/_redirects"]
+EXTRA_PATH_METADATA = {"extra/_redirects": {"path": "_redirects"}}
+
+if os.environ.get("CONTEXT") == "production":
+    STATIC_PATHS.append("extra/robots.txt")
+    EXTRA_PATH_METADATA["extra/robots.txt"] = {"path": "robots.txt"}
+else:
+    STATIC_PATHS.append("extra/robots_deny.txt")
+    EXTRA_PATH_METADATA["extra/robots_deny.txt"] = {"path": "robots.txt"}
+
+TAG_SAVE_AS = ""
+AUTHOR_SAVE_AS = ""
+CATEGORY_SAVE_AS = ""
+USE_SHORTCUT_ICONS = True
+
+# Elegant Labels
+SOCIAL_PROFILE_LABEL = "Stay in Touch"
+RELATED_POSTS_LABEL = "Keep Reading"
+SHARE_POST_INTRO = "Like this post? Share on:"
+COMMENTS_INTRO = "So what do you think? Did I miss something? Is any part unclear? Leave your comments below."
+
+# Email Subscriptions
+EMAIL_SUBSCRIPTION_LABEL = "Get New Release Alert"
+EMAIL_FIELD_PLACEHOLDER = "Enter your email..."
+SUBSCRIBE_BUTTON_TITLE = "Notify me"
+
+FREELISTS_NAME = "oracle-l"
+FREELISTS_FILTER = True
+
+# Legal
+SITE_LICENSE = """
+    Content licensed under <a rel="license nofollow noopener noreferrer"
+    href="http://creativecommons.org/licenses/by/4.0/" target="_blank">
+    Creative Commons Attribution 4.0 International License</a>.
+    """
+HOSTED_ON = {"name": "Github Pages", "url": "https://pages.github.com/"}
+
+
+# Landing Page
+PROJECTS_TITLE = "Recent Repositories"
+PROJECTS = [
+      {
+        "name": "wox_ext",
+        "url": "https://github.com/Weilet/wox_ext",
+        "description": "self-made wox extension",
+    },
+    {
+        "name": "LBlog",
+        "url": "https://github.com/Weilet/LBlog",
+        "description": "a light blog powered by flask",
+    },
+]
+
+LANDING_PAGE_TITLE = "Recent Activities"
+
+AUTHORS = {
+    "Talha Mansoor": {
+        "url": "https://www.oncrashreboot.com/",
+        "blurb": "is the creator and lead developer of Elegant theme.",
+        "avatar": "/images/avatars/talha131.png",
+    },
+    "Pablo Iranzo Gómez": {
+        "url": "http://iranzo.github.io",
+        "blurb": " opensource enthusiast and Lego fan doing some python simple programs like @redken_bot in telegram, etc",
+        "avatar": "https://avatars.githubusercontent.com/u/312463",
+    },
+    "Jack De Winter": {
+        "url": "http://jackdewinter.github.io",
+        "blurb": "ever evolving, ever learning",
+    },
+    "Matija Šuklje": {
+        "url": "https://matija.suklje.name",
+        "blurb": "FOSS lawyer by trade, hacker by heart.",
+    },
+}
+DISQUS_FILTER = True
+UTTERANCES_FILTER = True
